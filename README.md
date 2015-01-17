d96a39d89c53386e9bb414f4c213315a09b47716
deemind <deemin@gmail.com>
2015-01-17 18:10:04 +0530

    converted to a console frienly version

--- README.md
+++ README.md
@@ -6,14 +6,28 @@ Email: deemind@gmail.com
 Description: Outputs checkboard for given board size and square size.
 Language: PHP
 
-The program will print a table, given rows and columns are equal(boardSize*boardSize) and each TD element of the table
-will fill up with empty text data or hash mark data according to the board cell position (Odd or Even). 
+The program will display hash symbols and empty spaces, one set after other, according to the given board size. 
 
 For an example, lets assume boardsize = 3, and square size = 2
 
-123
-456
-789
+cells are numbered like,
+
+|1|2|3|
+|6|5|4|
+|7|8|9|
 
 Even number positions will filled up with hash symbols ,
-and Odd number positions will filled up with empty string data.
+and Odd number positions will filled up with empty string data / or star symbols.
+
+Example output:
+
++ - - + - - + - - + 
+| # # | * * | # # | 
+| # # | * * | # # | 
++ - - + - - + - - + 
+| * * | # # | * * | 
+| * * | # # | * * | 
++ - - + - - + - - + 
+| # # | * * | # # | 
+| # # | * * | # # | 
++ - - + - - + - - +
--- index.php
+++ index.php
@@ -4,75 +4,37 @@ class Checkerboard
 {
 	private $boardSize;
 	private $squareSize;
+	private $rowCount = 0;
+	private $cellCount = 0;
+
 	
 	public function __construct($sSize = 1, $bSize = 1){
+
 		$this->boardSize = $bSize;
 		$this->squareSize = $sSize;
 	}
 
 	public function drawBoard(){
-	
-		if($this->boardSize > 0 && $this->squareSize > 0){
-			echo "<table>";
-			$cellCount = 0;
-			$rowCount = 0;
 
-			for ($i=0; $i < $this->boardSize; $i++) {
-			
-				++$rowCount;
-			
-				echo "<tr><td colspan = $this->boardSize>".$this->drawRowSeperator()."</td></tr>";
-				echo "<tr>"; 
-		
-				if($rowCount % 2 == 0)
-				{
-					$cellCount = $this->boardSize * $rowCount;
-					
-					for ($j=$cellCount ; $j > (($rowCount - 1) * $this->boardSize); $j--) { 
-	
-						if($j % 2 == 0){
-							echo "<td>"; 
-							echo $this->drawInnerPattern(true);
-							echo "</td>"; 
-						}
-						else
-						{
-							echo "<td>"; 
-							echo $this->drawInnerPattern(false);
-							echo "</td>"; 
-						}						
-					}
-				}
-				else
-				{
-					for ($k=$cellCount  + 1; $k < $this->boardSize * $rowCount + 1; $k++) { 
+		for ($i=0; $i < $this->boardSize; $i++) { 
 
-						++$cellCount;
+			echo $this->drawRowSeperator();	
+			echo nl2br("\n");
 
-						if($k % 2 == 0){
-							echo "<td>"; 
-							echo $this->drawInnerPattern(true);
-							echo "</td>"; 
-						}
-						else
-						{
-							echo "<td>"; 
-							echo $this->drawInnerPattern(false);
-							echo "</td>"; 
-						}
-					}
-				}
-				echo "<tr/>";	
+			$this->rowCount++;
+
+			for ($j=0; $j < $this->squareSize; $j++) { 
+					echo $this->drawInnerPattern();
+					echo nl2br("\n");
 			}
-			echo "<tr><td colspan = $this->boardSize>".$this->drawRowSeperator()."</td></tr>";
+	
 		}
-		else
-		{
-			echo "You should have a valid combination of inputs !";
-		}
+		echo $this->drawRowSeperator();	
+		
 	}
 
 	private function drawRowSeperator(){
+
 		$bSize = $this->boardSize;
 		$sSize = $this->squareSize;
 
@@ -89,30 +51,70 @@ class Checkerboard
 		return $seperatorStr;
 	}
 
-	private function drawInnerPattern($fillEmpty = false){
+	private function drawInnerPattern(){
+
 		$sSize = $this->squareSize;
 		$innerStr = "";
+		$space = " * "; 
 
-		if(!$fillEmpty){
-			for ($i=0; $i < $sSize; $i++) { 
-				$innerStr .= "| ";
-				for ($j=0; $j < $sSize; $j++) {
-					$innerStr .= "#";
-				}
-				$innerStr .= "<br/>";
-			}
+		$bSize = $this->boardSize;
+		$sSize = $this->squareSize;
 
+		if($this->rowCount % 2 == 0)
+		{
+			$this->cellCount = $bSize*$this->rowCount;	
 		}
 		else
-		{
-			for ($i=0; $i < $sSize; $i++) { 
-				$innerStr .= "| ";
-				for ($j=0; $j < $sSize; $j++) {
-					$innerStr .= " ";
-				}
-				$innerStr .= "<br/>";
-			}
+		{			
+			$this->cellCount = ($this->rowCount-1) * $bSize + 1;
 		}
+
+		for ($i=0; $i < $bSize; $i++) { 
+		
+			$innerStr.="|";
+
+				if($this->rowCount % 2 == 0)
+				{									
+					if($this->cellCount % 2 == 0)
+					{					
+						for ($j=0; $j < $sSize; $j++) {
+							
+							$innerStr.= $space;
+						} 
+					}
+					else
+					{						
+						for ($k=0; $k < $sSize; $k++) {
+						
+							$innerStr.=" # ";
+						} 
+					}
+					$this->cellCount--;
+				}
+				else
+				{	
+
+					if($this->cellCount % 2 == 0)
+					{						
+						for ($l=0; $l < $sSize; $l++) {
+							
+							$innerStr.= $space;
+						} 
+					}
+					else
+					{					
+						for ($m=0; $m < $sSize; $m++) {
+						
+							$innerStr.=" # ";
+						} 
+					}
+					$this->cellCount++;
+				}
+			
+		}
+
+		$innerStr.=" | ";
+
 		return $innerStr;
 	}
 }
